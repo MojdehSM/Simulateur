f84b87b2a6917160eb16c7b5c1c0bac2fadeeb7e
MojdehSM <mojde.mohamadi@yahoo.com>
2014-03-31 23:11:27 +0200

    find

--- MaritimeSimulator/gen/com/intactile/R.java
+++ MaritimeSimulator/gen/com/intactile/R.java
@@ -24,8 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080004;
         public static final int button1=0x7f080001;
+        public static final int button2=0x7f080002;
+        public static final int button3=0x7f080003;
         public static final int textView1=0x7f080000;
     }
     public static final class layout {
@@ -33,11 +35,13 @@ public final class R {
     }
     public static final class menu {
         public static final int maritime=0x7f070000;
+        public static final int show=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_show=0x7f050003;
     }
     public static final class style {
         /** 
--- MaritimeSimulator/src/com/intactile/MaritimeActivity.java
+++ MaritimeSimulator/src/com/intactile/MaritimeActivity.java
@@ -1,24 +1,46 @@
 package com.intactile;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Calendar;
+import java.util.Random;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MaritimeActivity extends Activity implements OnClickListener {
-	
-	Button bt;
 
+	Button bt;
+	Button bt2;
+	Button bt3;
+
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_maritime);
-		
-		bt = (Button)findViewById(R.id.button1);
+
+		bt = (Button) findViewById(R.id.button1);
 		bt.setOnClickListener(this);
+
+		bt2 = (Button) findViewById(R.id.button2);
+		bt2.setOnClickListener(this);
+
+		bt3 = (Button) findViewById(R.id.button3);
+		bt3.setOnClickListener(this);
 	}
 
 	@Override
@@ -30,8 +52,63 @@ public class MaritimeActivity extends Activity implements OnClickListener {
 
 	@Override
 	public void onClick(View arg0) {
-		Intent serviceGeoSender = new Intent(this, SimulatorService.class);
-		startService(serviceGeoSender);
+		if (arg0 == bt) {
+			Intent serviceGeoSender = new Intent(this, SimulatorService.class);
+			startService(serviceGeoSender);
+		} else if (arg0 == bt3) {
+			SimulatorService.currentIdentifier = new Random().nextLong();
+			
+			Toast.makeText(this, "New Identifier created "+SimulatorService.currentIdentifier, Toast.LENGTH_LONG).show();
+			
+		} else {
+			try {
+				FileInputStream fIn = openFileInput("location.csv");
+
+				String line;
+				StringBuffer sb = new StringBuffer();
+
+				BufferedReader reader = new BufferedReader(new InputStreamReader(fIn));
+
+				while ((line = reader.readLine()) != null) {
+					Log.e("TEST", line);
+					sb.append(line + "\n");
+				}
+
+				fIn.close();
+
+				Intent email = new Intent(Intent.ACTION_SEND);
+				email.putExtra(Intent.EXTRA_EMAIL, new String[] { "mohamadi.mojde@yahoo.com" });
+				email.putExtra(Intent.EXTRA_SUBJECT, "Maritime Simulation");
+				email.putExtra(Intent.EXTRA_TEXT, sb.toString());
+				email.setType("message/rfc822");
+				startActivity(Intent.createChooser(email, "Choisissez votre Client Email  :"));
+
+				deleteFile("location.csv");
+				FileOutputStream fOut2 = openFileOutput("location.csv",MODE_APPEND);
+				fOut2.close();
+				
+				String  name = Calendar.getInstance().getTime().toString()+".csv";
+				FileOutputStream fOut = openFileOutput(name,MODE_APPEND);
+				OutputStreamWriter osw = new OutputStreamWriter(fOut);
+				osw.write(sb.toString());
+				fOut.flush();
+				fOut.close();
+				
+				for(File ctx : getFilesDir().listFiles()){
+					Log.e("FILE", ctx.getName());
+				}
+
+				Toast.makeText(this, "Data Saved "+SimulatorService.currentIdentifier, Toast.LENGTH_LONG).show();
+				
+					
+				
+				
+			} catch (IOException ex) {
+				Log.e("ERROR",ex.getMessage());
+				ex.printStackTrace();
+			}
+
+		}
 	}
-	
+
 }
